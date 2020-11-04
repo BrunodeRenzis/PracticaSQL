0198aa219cd236bf617d6e6863038e85ce3e9e5e
Bruno de Renzis <brunoderenzisrubinetti26@gmail.com>
2020-11-04 15:54:41 -0300

    Práctica manipulación base de datos

---
+++ PracticaSQL/PrácticaSQL/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrácticaSQL
+{
+    public class Persona
+    {
+        string apellido;
+        int dni;
+        int id;
+        string nombre;
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string apellido, int dni, int id, string nombre):this()
+        {
+            this.Apellido = apellido;
+            this.Dni = dni;
+            this.Id = id;
+            this.Nombre = nombre;
+        }
+
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Nombre del seleccionado {this.Nombre}");
+            sb.AppendLine($"Apellido del seleccionado {this.Apellido}");
+            sb.AppendLine($"Dni del seleccionado {this.Dni}");
+            sb.AppendLine($"ID del seleccionado {this.Id}");
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ PracticaSQL/PrácticaSQL/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace PrácticaSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conexion = "Data Source=agasoluciones.dynamic-dns.net\\mssqlserver2; Initial Catalog = Mensajes; User Id=Alumno;Password=FraUtn;";
+            SqlConnection miConexion = new SqlConnection();
+            string nombreAlumno = String.Empty;
+            string apellidoAlumno= String.Empty;
+            string dniAlumno=String.Empty;
+            string idAlumno=String.Empty;
+            miConexion.ConnectionString=conexion;
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = miConexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "select * from Personas where apellido='Noguera'";
+            miConexion.Open();
+            SqlDataReader lector = comando.ExecuteReader();
+            while(lector.Read())
+            {
+                nombreAlumno = lector["Nombre"].ToString();
+                apellidoAlumno = lector["Apellido"].ToString();
+                dniAlumno = lector["DNI"].ToString();
+                idAlumno  = lector["Id"].ToString();
+            }
+            Persona unaPersona = new Persona(apellidoAlumno,int.Parse(dniAlumno),int.Parse(idAlumno),nombreAlumno);
+            lector.Close();
+            comando.CommandText = "insert into Personas (Nombre,Apellido,DNI) values('Bruno', 'de Renzis',39104689)";
+            comando.ExecuteNonQuery();
+
+            Console.WriteLine(unaPersona.ToString());
+            Console.ReadKey();
+            
+        }
+    }
+}
